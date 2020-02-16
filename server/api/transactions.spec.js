8bbb4c9bef78ed072f7b93992497fcae1cb5a063
Sergei Shneider <shneider.s@gmail.com>
2020-02-15 23:08:51 -0500

    test

---
+++ server/api/transactions.spec.js
@@ -0,0 +1,11 @@
+
+const {expect} = require('chai')
+const request = require('supertest')
+const db = require('../db')
+const app = require('../index')
+const User = db.model('user')
+const Trans = db.model('transaction')
+const seed = require('../../script/seed')
+describe('transactionTests - api', ()=>{
+    
+})
